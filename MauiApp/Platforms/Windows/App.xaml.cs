6f4e32d1f4147c20459ce34ed4bddd856ff4330f
Dennise Catolos <dentolos19@gmail.com>
2022-08-31 17:51:40 +0800

    Fixed .NET MAUI project

--- MauiApp/Platforms/Windows/App.xaml.cs
+++ MauiApp/Platforms/Windows/App.xaml.cs
@@ -19,6 +19,6 @@ namespace MauiApp.WinUI
             this.InitializeComponent();
         }
 
-        protected override MauiApp CreateMauiApp() => MauiProgram.CreateMauiApp();
+        protected override Microsoft.Maui.Hosting.MauiApp CreateMauiApp() => MauiProgram.CreateMauiApp();
     }
 }
